6fe580af5d3919bdceb490153f3c41b5cf962196
1481980097@qq.com <1481980097@qq.com>
2019-05-03 10:52:48 +0800

    更改feign接口

--- account-feignClient/src/main/java/top/liuliyong/account/client/AccountClient.java
+++ account-feignClient/src/main/java/top/liuliyong/account/client/AccountClient.java
@@ -1,8 +1,10 @@
 package top.liuliyong.account.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import top.liuliyong.account.common.response.AccountOperationResponse;
 
 /**
@@ -14,5 +16,8 @@ public interface AccountClient {
 
     @PostMapping("/account/logincheck")
     AccountOperationResponse logincheck(@RequestHeader("session_id") String sessionId);
+
+    @GetMapping("/account")
+    AccountOperationResponse findAccountByAccountId(@RequestHeader String session_id, @RequestParam String account_id);
 
 }
