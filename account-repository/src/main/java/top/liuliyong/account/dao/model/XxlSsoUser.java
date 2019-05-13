5b28c5506efe5509980d30e84840f3b3120e1f9a
1481980097@qq.com <1481980097@qq.com>
2019-05-13 15:38:30 +0800

    Initializing repository module

---
+++ account-repository/src/main/java/top/liuliyong/account/dao/model/XxlSsoUser.java
@@ -0,0 +1,26 @@
+package top.liuliyong.account.dao.model;
+
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * xxl sso user
+ *
+ * @author xuxueli 2018-04-02 19:59:49
+ */
+@Data
+public class XxlSsoUser extends Account {
+    private static final long serialVersionUID = 42L;
+
+    // field
+    private String userid;
+    private String username;
+    private Map<String, String> plugininfo;
+
+    private String version;
+    private int expireMinite;
+    private long expireFreshTime;
+
+
+}
